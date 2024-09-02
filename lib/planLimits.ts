d50b3a33c8668ac3826c370d0844bbb5cf75930f
SebastianEscudero <sebastin.escudero@uc.cl>
2024-09-02 09:57:06 -0400

    new pricing page + contact form

--- lib/planLimits.ts
+++ lib/planLimits.ts
@@ -4,21 +4,27 @@ export function getMaxBoards(org: any): number {
   const plan = org.subscriptionPlan;
 
   const planDetails = plans.find(subscriptionPlan => subscriptionPlan.label === plan);
-  
+
   if (org.subscription) {
     if (new Date(org.subscription.mercadoPagoCurrentPeriodEnd).getTime() < new Date().getTime()) {
       return 1;
     }
   }
 
-  return planDetails ? (planDetails.features.Boards === "Ilimitados" ? Infinity : Number(planDetails.features.Boards)) : 0;
-}
+  if (planDetails && planDetails.features) {
+    return planDetails.features.Boards === "Ilimitados" ? Infinity : Number(planDetails.features.Boards);
+  }
+  return 0;
+};
 
 export function getMaxCapas(org: any): number {
   const plan = org.subscriptionPlan;
 
   const planDetails = plans.find(subscriptionPlan => subscriptionPlan.label === plan);
-  return planDetails ? (planDetails.features["Capas máximas"] === "Ilimitados" ? Infinity : Number(planDetails.features["Capas máximas"])) : 0;
+  if (planDetails && planDetails.features) {
+    return planDetails.features["Capas máximas"] === "Ilimitados" ? Infinity : Number(planDetails.features["Capas máximas"]);
+  }
+  return 0;
 }
 
 export function getMaxOrganizations(user: any): number {
@@ -29,8 +35,11 @@ export function getMaxOrganizations(user: any): number {
     let planLimit = 1
     if (org.subscription) {
       if (new Date(org.subscription.mercadoPagoCurrentPeriodEnd).getTime() > new Date().getTime()) {
-        planLimit = planDetails ? (planDetails.features["Teams"] === "Ilimitados" ? Infinity : Number(planDetails.features["Teams"])) : 0;
-    }}
+        if (planDetails && planDetails.features) {
+          planLimit = planDetails.features["Teams"] === "Ilimitados" ? Infinity : Number(planDetails.features["Teams"]);
+        }
+      }
+    }
     maxOrganizations = Math.max(maxOrganizations, planLimit);
   }
   return maxOrganizations;
@@ -40,10 +49,10 @@ export function getMaxImageSize(org: any): number {
   const plan = org.subscriptionPlan;
 
   const planDetails = plans.find(subscriptionPlan => subscriptionPlan.label === plan);
-  if (planDetails) {
+  if (planDetails && planDetails.features) {
     const imageSizeStr = planDetails.features["Imágenes"];
-    const match = imageSizeStr.match(/\d+/); 
-    return match ? Number(match[0]) : 0; 
+    const match = imageSizeStr.match(/\d+/);
+    return match ? Number(match[0]) : 0;
   }
   return 0;
 }
