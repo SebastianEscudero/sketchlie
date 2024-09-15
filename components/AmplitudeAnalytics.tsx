755004a6ef0c9212f5930ffcf68dd0eede737f98
SebastianEscudero <sebastin.escudero@uc.cl>
2024-09-15 14:52:52 -0300

    amplitude analytics only in prod

--- components/AmplitudeAnalytics.tsx
+++ components/AmplitudeAnalytics.tsx
@@ -8,23 +8,25 @@ const API_KEY = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY!;
 
 export function AmplitudeAnalytics() {
   useEffect(() => {
-    console.log('Initializing Amplitude...');
-    amplitude.init(API_KEY, 'session replay user', {
-      defaultTracking: {
-        sessions: true,
-        pageViews: true,
-        formInteractions: true,
-        fileDownloads: true,
-      },
-    });
+    if (process.env.NODE_ENV === 'production') {
+      console.log('Initializing Amplitude...');
+      amplitude.init(API_KEY, 'session replay user', {
+        defaultTracking: {
+          sessions: true,
+          pageViews: true,
+          formInteractions: true,
+          fileDownloads: true,
+        },
+      });
 
-    const sessionReplayTracking = sessionReplayPlugin({
-      sampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1, // 10% in production, 100% in development
-    });
+      const sessionReplayTracking = sessionReplayPlugin({
+        sampleRate: 0.1, // 10% in production
+      });
 
-    amplitude.add(sessionReplayTracking);
+      amplitude.add(sessionReplayTracking);
 
-    console.log('Amplitude initialized with session replay');
+      console.log('Amplitude initialized with session replay');
+    }
   }, []);
 
   return null;
