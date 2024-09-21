d297417a75fc777df73d110d9b1ce0b83a4602d7
SebastianEscudero <sebastin.escudero@uc.cl>
2024-09-21 19:22:49 -0300

    international format for sketchlie

--- lib/dictionaries.ts
+++
@@ -1,7 +0,0 @@
-import 'server-only';
-
-export const dictionaries = {
-    en: () => import('@/public/locales/en.json').then(module => module.default),
-    pt: () => import('@/public/locales/pt.json').then(module => module.default),
-    es: () => import('@/public/locales/es.json').then(module => module.default),
-}
