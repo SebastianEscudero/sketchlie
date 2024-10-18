f7c93f871bac954cf8aacdf222ec3f1e1e035bf0
SebastianEscudero <sebastin.escudero@uc.cl>
2024-10-18 19:07:05 -0300

    added by label now dynamically shows based on viewport width

--- app/board/[boardId]/_components/added-layer-by-label.tsx
+++ app/board/[boardId]/_components/added-layer-by-label.tsx
@@ -8,7 +8,7 @@ export const AddedLayerByLabel = memo(({ addedByLabel }: AddedLayerByLabelProps)
     if (!addedByLabel) return null;
 
     return (
-        <div className="absolute bottom-8 left-8 text-zinc-500 dark:text-zinc-400 text-sm">
+        <div className="absolute lg:block hidden bottom-8 left-8 text-zinc-500 dark:text-zinc-400 text-sm">
             Added by <span className="font-bold">{addedByLabel}</span>
         </div>
     )
