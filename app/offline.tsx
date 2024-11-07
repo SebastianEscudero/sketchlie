e5e8eca319ce047f42e59eb3b388849919fe775e
SebastianEscudero <sebastin.escudero@uc.cl>
2024-11-07 11:32:35 -0300

    fix(apple-touch-icon)

--- app/offline.tsx
+++ app/offline.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useEffect } from 'react'
 import { LandingNavbar } from '@/components/landing-navbar'
 import { BotNavbar } from '@/components/bottom-navbar'
 import { mainFont } from '@/lib/font'
@@ -9,29 +8,27 @@ import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
-export default function Error({
-    error,
-    reset,
-}: {
-    error: Error & { digest?: string }
-    reset: () => void
-}) {
-    useEffect(() => {
-        // Log the error to an error reporting service
-        console.error(error)
-    }, [error])
-
+export default function Offline() {
     return (
         <main className={cn("bg-white", mainFont.className)} lang="es">
             <div className="mx-auto h-full">
                 <LandingNavbar />
                 <div className="flex flex-col items-center justify-center h-screen">
-                    <Image src="/placeholders/error.svg" alt="Error" width={100} height={100} className='w-1/2 h-1/2' />
-                    <h1 className="text-4xl font-bold mt-4">Oops! Something went wrong</h1>
-                    <p className="text-lg mt-4">Please try again later</p>
+                    <Image 
+                        src="/placeholders/offline.svg" 
+                        alt="No internet connection" 
+                        width={100} 
+                        height={100} 
+                        className='w-1/2 h-1/2' 
+                    />
+                    <h1 className="text-4xl font-bold mt-4">No internet connection</h1>
+                    <p className="text-lg mt-4">Check your connection and try again</p>
                     <div className="flex gap-4 mt-6">
-                        <Button variant="outline" onClick={reset}>
-                            Try again
+                        <Button 
+                            variant="outline" 
+                            onClick={() => window.location.reload()}
+                        >
+                            Refresh page
                         </Button>
                         <Button variant="sketchlieBlue" asChild>
                             <Link href="/">Go to home</Link>
@@ -39,7 +36,6 @@ export default function Error({
                     </div>
                 </div>
                 <BotNavbar />
-
             </div>
         </main>
     )
